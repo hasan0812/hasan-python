30a0c844f3534f8b324e7844763dcfa542f15646
hasan0812 <somiakhan86@gmail.com>
2025-05-27 11:01:29 -0700

    a1

--- lesson1/main.py
+++ lesson1/main.py
@@ -1,1 +1,10 @@
 print("hello world")
+print("Welcome To Codingal")
+name=input("What is your name?")
+print("hello",name)
+if name=="Hasan":
+    [print("Welcome Hasan")
+else:
+print("Get Out")]
+
+
