6cbfc188b142b474efdff477eca9865308045725
hasan0812 <somiakhan86@gmail.com>
2025-05-27 11:37:43 -0700

    a1

--- lesson1/main.py
+++ lesson1/main.py
@@ -6,15 +6,18 @@ print("hello",name)
   #  [print("Welcome Hasan")
 #]
 #else:[print("Get Out")]
-if name.upper()=="HASAN":
-[print("Welcome HASAN")]
-else
-[
-print("Get out")]
-age=int(input{"enter your age:"})
+#if name.upper()=="HASAN":
+#[print("Welcome HASAN")]
+#else
+#[
+#print("Get out")]
+age=int(input("enter your age:"))
+print(age)
 if age>=18:
 [print("you can vote")]
 else
-[print("you can not vote")]
+[
+print("you can not vote")
+]
 
 
