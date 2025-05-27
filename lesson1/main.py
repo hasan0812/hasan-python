65a5b984835024873cfea59b4ce8955cba53c5ea
hasan0812 <somiakhan86@gmail.com>
2025-05-27 11:22:27 -0700

    a2

--- lesson1/main.py
+++ lesson1/main.py
@@ -1,10 +1,20 @@
-print("hello world")
-print("Welcome To Codingal")
+#print("hello world")
+#print("Welcome To Codingal")
 name=input("What is your name?")
 print("hello",name)
-if name=="Hasan":
-    [print("Welcome Hasan")
-else:
-print("Get Out")]
+#if name=="Hasan":
+  #  [print("Welcome Hasan")
+#]
+#else:[print("Get Out")]
+if name.upper()=="HASAN":
+[print("Welcome HASAN")]
+else
+[
+print("Get out")]
+age=int(input{"enter your age:"})
+if age>=18:
+[print("you can vote")]
+else
+[print("you can not vote")]
 
 
