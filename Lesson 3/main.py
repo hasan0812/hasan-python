3d540e754fc89f2eb197d47b9acc0abb897b91cf
hasan0812 <somiakhan86@gmail.com>
2025-05-28 06:37:57 -0700

    A3

---
+++ Lesson 3/main.py
@@ -0,0 +1,29 @@
+#loop on array
+fruits = ["Apple","banana","cherry"]
+for x in fruits:
+    print(x)
+   
+ #char loop
+name = "python"
+for char in name:
+    print(char)
+
+#while loop
+count = 0
+while count <5:
+    print(count)
+    count += 1
+
+for i in range(1,4):
+    for j in range(1,4):
+        print ("*",end=" ")
+        print(" ")
+
+#print multiplication table
+for i in range(1,6): #outer loop for rows
+    for j in range(1,6): # inner loop for columns
+        print(f "{i * j}",end = "/t")
+        print() #move to next line
+
+
+    
