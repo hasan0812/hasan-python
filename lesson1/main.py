5216ef1a9ca1cff1b3c6f9c61b063b72d09b4c40
hasan0812 <somiakhan86@gmail.com>
2025-05-28 00:01:47 -0700

    a2

--- lesson1/main.py
+++ lesson1/main.py
@@ -1,4 +1,4 @@
-print("hello world")
+#print("hello world")
 print("Welcome To Codingal")
 name=input("What is your name?")
 print("hello",name)
@@ -24,5 +24,31 @@ num=int(input{"enter a number"})
 if num%2==0:
 [print(f"{num}is even num")]
 else
-[prin(f"{num}is odd num")]
-
+[print(f"{num}is odd num")]
+number=int(input {"enter a number"})
+if number > 50 :
+[print("number is greater than 50")]
+if number% 2==0 :
+[print("even")]
+else
+[print("odd")]
+else
+[print("number is smaller than 50")]
+newnum=int(input{"enter favourite number"})
+if newnum<=50 :
+[print("number is less than 50 and is,")]
+if newnum% 2==0 :
+[print("even")]
+else
+[print("odd")]
+else
+[print ("too big number")]
+marks=int(input{"tell me your marks"})
+if marks>= 90 :
+[print("grade A")]
+elif marks>=75 :
+print("grade B")
+elif marks>=50
+print("grade C")
+else :
+[print("grade F")]
