835fe1c5a173a38a84b67c6996f7ea61d76bfb3d
hasan0812 <somiakhan86@gmail.com>
2025-06-12 06:15:47 -0700

    q3

--- Variables an data types.py
+++ Variables an data types.py
@@ -1,18 +1,28 @@
 #WAP to input 2 numbers and print sum.
-a = int(input("Enter first number"))
-b = int(input("Enter second number"))
-sum = a + b
-print("sum is ",sum)
+# a = int(input("Enter first number"))
+# b = int(input("Enter second number"))
+# sum = a + b
+# print("sum is ",sum)
 
 #WAP to input 2 numbers and print difference
-H1 = int(input("Enter first number"))
-H2 = int(input("Enter second number"))
-if(H1 >= H2):
-    diff = H1 - H2
-    print("diff is ",diff)
-elif(H2 > H1):
-    diff = H1 - H2
-    print("diff is ",diff)
+# H1 = int(input("Enter first number"))
+# H2 = int(input("Enter second number"))
+# if(H1 >= H2):
+#     diff = H1 - H2
+#     print("diff is ",diff)
+# elif(H2 > H1):
+#     diff = H2 - H1
+#     print("diff is ",diff)
+# else:
+#     print("activity end")   
+
+#WAP to input a number and print it is even or odd
+
+h = int(input("Enter any number"))
+num = h % 2
+if(num == 0):
+    print("even number is:",h)
 else:
-    print("activity end")      
+    print("odd number is:",h)
+print("program is end")   
 
