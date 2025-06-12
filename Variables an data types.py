6691c1c9bd25fabae8435fb73ad270562d1260b6
hasan0812 <somiakhan86@gmail.com>
2025-06-12 05:54:41 -0700

    q1

---
+++ Variables an data types.py
@@ -0,0 +1,7 @@
+#WAP to input 2 numbers and print sum.
+a = int(input("Enter first number"))
+b = int(input("Enter second number"))
+sum = a + b
+print("sum is ",sum)
+
+#WAP to input 2 numbers and print difference
