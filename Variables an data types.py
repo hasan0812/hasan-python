ecdcc90ad4f7545b0a746de594507ffa5f3f049f
hasan0812 <somiakhan86@gmail.com>
2025-06-12 06:03:36 -0700

    q2

--- Variables an data types.py
+++ Variables an data types.py
@@ -5,3 +5,14 @@ sum = a + b
 print("sum is ",sum)
 
 #WAP to input 2 numbers and print difference
+H1 = int(input("Enter first number"))
+H2 = int(input("Enter second number"))
+if(H1 >= H2):
+    diff = H1 - H2
+    print("diff is ",diff)
+elif(H2 > H1):
+    diff = H1 - H2
+    print("diff is ",diff)
+else:
+    print("activity end")      
+
