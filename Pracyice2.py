e3c0ce3c1b25d013c829b45b5db0e788ab41f75b
hasan0812 <somiakhan86@gmail.com>
2025-06-25 06:46:51 -0700

    snake game

--- Pracyice2.py
+++ Pracyice2.py
@@ -1,13 +1,14 @@
-class Animal:
-    def __init__(self, name):
-        self.name = name
+# class Animal:
+#     def __init__(self, name):
+#         self.name = name
 
-    def eat(self):
-        print(f"{self.name} is eating")
+#     def eat(self):
+#         print(f"{self.name} is eating")
 
-    def walk(self):
-        print(f"{self.name} is walking")
+#     def walk(self):
+#         print(f"{self.name} is walking")
 
-dog = Animal("Dog")
-dog.eat()
-dog.walk()        
+# dog = Animal("Dog")
+# dog.eat()
+# dog.walk()
+    
